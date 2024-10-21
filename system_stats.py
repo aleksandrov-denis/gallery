4cec7b7a42245647f3de91a34e0513fcf0574818
Denis Aleksandrov <denisuzh@gmail.com>
2024-10-21 19:04:33 -0400

    Makes the webpage dynamic and stops system stats script when not on stats page
    Signed-off-by: Denis Aleksandrov <denisuzh@gmail.com>

--- system_stats.py
+++ system_stats.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import psutil
 
 app = Flask(__name__)
 
 @app.route('/')
-def system_stats():
+def index():
+        return render_template('stats.html')
+
+@app.route('/projects.html')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/api/stats')
+def api_stats():
     stats = {
         'cpu_percent': psutil.cpu_percent(interval=1),
-        'memory': psutil.virtual_memory(),
-        'disk': psutil.disk_usage('/')
+        'memory': psutil.virtual_memory().percent,
+        'disk': psutil.disk_usage('/').percent
     }
-    return render_template('stats.html', stats=stats)
+    return jsonify(stats)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
